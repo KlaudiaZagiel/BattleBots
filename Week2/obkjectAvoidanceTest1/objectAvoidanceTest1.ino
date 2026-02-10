fc7877dd27522fe9417803379aaafd995318ca71
JessicaC <jessica.fernandes.camacho@student.nhlstenden.com>
2026-02-10 11:11:16 +0100

    Create objectAvoidanceTest1.ino
    Preparation of a testing code to test once I have the robot and we connect the ultrasonic sensor. To see it working we place a hand in front of the sensor, the closer it gets, it should show numbers closer to zero, if it shows always 0 regardless, it means the sensor is not connected properly.

---
+++ Week2/obkjectAvoidanceTest1/objectAvoidanceTest1.ino
@@ -0,0 +1,33 @@
+const int TRIGGERPIN = 9;
+const int ECHOPIN = 6;
+
+long duration; // in microseconds
+float distance; // will store distance in cm
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+}
+
+void loop() 
+{
+  // Send trigger pulse
+  digitalWrite(TRIGGERPIN, HIGH);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIGGERPIN, LOW);
+  delayMicroseconds(10);
+
+  digitalWrite(TRIGGERPIN, HIGH);
+
+  duration = pulseIn(ECHOPIN, LOW);
+
+  distance = duration * 0.0343 / 2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  delay(500);
+}
