0c2f5d55106aaf873044ceced4fbce98c8c651b1
Klaudia <klaudia.zagiel@student.nhlstenden.com>
2026-03-05 15:52:54 +0100

    Update grippertest.ino
    Fixed the comments so they provide more clarity for eachother.

--- Week3/grippertest/grippertest.ino
+++ Week3/grippertest/grippertest.ino
@@ -1,5 +1,5 @@
-#define  SERVO          12  //GR stands for gripper
-#define  GRIPPER_OPEN   1600 // pulses needed for the gripper to be open (calculated previously)
+#define  SERVO          12  
+#define  GRIPPER_OPEN   1600 
 #define  GRIPPER_CLOSE  500
 const int MOTOR_1_BACKWARD = 11;
 const int MOTOR_1_FORWARD = 10;
@@ -8,14 +8,14 @@ const int MOTOR_2_BACKWARD = 6;
 const int MOTOR_1_PWM = 255;
 const int MOTOR_2_PWM = 255;
 const int BUTTON = 7;
-const int ROTATIONWHEEL1 = 3; //rotation sensor.
-const int ROTATIONWHEEL2 = 2;
-bool lastStateRotation2 = LOW; //this prevents counting the same pulse multiple times
+const int ROTATIONWHEEL1 = 3;      //rotation sensor 1
+const int ROTATIONWHEEL2 = 2;      //rotation sensor 2
+bool lastStateRotation2 = LOW;   //this prevents counting the same pulse multiple times
 bool lastStateRotation1 = LOW;
 bool buttonPressed = false;
-int pulseCount2 = 0;
-int pulseCount1 = 0;
-int stage = 1;
+int pulseCount2 = 0;       //counts pulses from rotation sensor 2
+int pulseCount1 = 0;      //counts pulses from rotation sensor 1
+int stage = 1;           //controls the state machine of the robot
 
 
 void setup() {
@@ -42,7 +42,8 @@ void loop() {
 
     //Switch case//
     switch(stage){
-      
+
+    //Start moving//
     case 1:
     if(buttonPressed == true){
         if(pulseCount1 <= 24 && pulseCount2 <= 24) {
@@ -56,7 +57,8 @@ void loop() {
     }
 
     break;  
-    
+
+    //Wait 1 second//
     case 2:
     static unsigned long startTime = millis();
     if(millis() - startTime >= 1000) {
@@ -64,6 +66,7 @@ void loop() {
     }
     break;
 
+    //Move again//
     case 3:
     if(pulseCount1 <= 24 && pulseCount2 <= 24) {
             startMotorsForward();
@@ -75,17 +78,17 @@ void loop() {
 }
 
 
-void gripper(int newPulse) { //this will be our 1600(gripper open)(in this case)
-    static unsigned long timer;
-    static int pulse;
-    if(millis() > timer) { //timer is always 20ms greater than millis ensuring that u wait those 20ms
-        if(newPulse > 0) {
+void gripper(int newPulse) { 
+    static unsigned long timer;   //stores timing between pulses
+    static int pulse;            //stores the pulse duration
+    if(millis() > timer) {      //checks if it's time to send a new servo pulse
+        if(newPulse > 0) {     //updates pulses if a new value is provided
             pulse = newPulse;
         }
         digitalWrite(SERVO, 1);
-        delayMicroseconds(pulse);
+        delayMicroseconds(pulse);   //keep it high for the needed pulse duration
         digitalWrite(SERVO, 0);
-        timer = millis() + 20; //20 is in ms, this time is needed for the servo to process before receiving a new signal
+        timer = millis() + 20;     //servo signal repeats every 20ms
     }
 }
 
@@ -97,14 +100,16 @@ void startMotorsForward() {
 }
 
 void pulseCounter() {
-    bool currentStateRotation2 = digitalRead(ROTATIONWHEEL2); //reads sensor 2
-        if(currentStateRotation2 == HIGH && lastStateRotation2 == LOW ){ //pulse detection - by comparing previous state, it prevents multiple pulse counts
+
+    //Rotation wheel sensor state//
+    bool currentStateRotation2 = digitalRead(ROTATIONWHEEL2);                //reads sensor 2
+        if(currentStateRotation2 == HIGH && lastStateRotation2 == LOW ){    //pulse detection - by comparing previous state, it prevents multiple pulse counts
             pulseCount2++; 
          }
     lastStateRotation2 = currentStateRotation2; //
 
-    bool currentStateRotation1 = digitalRead(ROTATIONWHEEL1);
-        if(currentStateRotation1 == HIGH && lastStateRotation1 == LOW ){  
+    bool currentStateRotation1 = digitalRead(ROTATIONWHEEL1);              //reads sensor 1
+        if(currentStateRotation1 == HIGH && lastStateRotation1 == LOW ){  //pulse detection - by comparing previous state, it prevents multiple pulse counts
              pulseCount1++;
          }
     lastStateRotation1 = currentStateRotation1;
